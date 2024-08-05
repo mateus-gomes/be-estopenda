f183da73c63f9e8cb3397ab309d1f152c5abe7e8
Mateus Gomes <magomescruz@gmail.com>
2024-08-05 09:10:11 -0300

    feat: create base project

---
+++ estopenda/src/main/java/com/mateus_gomes/estopenda/EstopendaApplication.java
@@ -0,0 +1,13 @@
+package com.mateus_gomes.estopenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstopendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstopendaApplication.class, args);
+	}
+
+}
---
+++ estopenda/src/test/java/com/mateus_gomes/estopenda/EstopendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus_gomes.estopenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstopendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
